efddcccc847f3277c09316a917b7a6d3bbb0d0fd
xihe <im0k@outlook.com>
2023-10-15 21:46:38 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import '@/styles/index.sass'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+
+const app = createApp(App)
+
+app.use(createPinia())
+
+app.mount('#app')
