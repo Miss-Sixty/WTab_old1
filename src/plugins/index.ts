83515f525beae7d90e455cd5b5aab0eb548548d7
xihe <im0k@outlook.com>
2023-10-22 22:30:06 +0800

    feat: 删除错误监控

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -11,22 +11,5 @@ import dayjs from 'dayjs'
 import 'dayjs/locale/zh-cn'
 dayjs.locale('zh-cn')
 
-
-import { inject } from '@vercel/analytics';
-inject();
-
-import { H } from 'highlight.run';
-H.init('6glr2klg', {
-    environment: 'production',
-    version: 'commit:abcdefg12345',
-	networkRecording: {
-		enabled: true,
-		recordHeadersAndBody: true,
-        urlBlocklist: [
-            // insert full or partial urls that you don't want to record here
-			// Out of the box, Highlight will not record these URLs (they can be safely removed):
-			"https://www.googleapis.com/identitytoolkit",
-			"https://securetoken.googleapis.com",
-        ],
-	},
-});
+import { inject } from '@vercel/analytics'
+inject()
