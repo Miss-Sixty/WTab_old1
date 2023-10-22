0bf8c0b95c7e474d29fa66a49970f628128e704f
xihe <im0k@outlook.com>
2023-10-22 21:28:56 +0800

    fix: 修改dayjs时区和语言

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,12 @@
 import '@/styles/index.scss'
-
-import 'simplebar'
-import 'simplebar/dist/simplebar.min.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-import { createHead } from 'unhead'
+import '@/plugins'
 
 import Button from '@/components/Button.vue'
 import Dialog from '@/components/Dialog.vue'
 
-createHead()
 const app = createApp(App)
 app.component('Button', Button)
 app.component('Dialog', Dialog)
---
+++ src/plugins/index.ts
@@ -0,0 +1,12 @@
+//meta
+import { createHead } from 'unhead'
+createHead()
+
+// 滚动条
+import 'simplebar'
+import 'simplebar/dist/simplebar.min.css'
+
+// dayjs
+import dayjs from 'dayjs'
+import 'dayjs/locale/zh-cn'
+dayjs.locale('zh-cn')
